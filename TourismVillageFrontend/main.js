b43ed0a10c2f63d2141857751e388a83c36d63ae
Yegon Kip <KY@Germanschool.local>
2024-02-05 16:22:49 +0300

    started, but still stuck on the turning caret/chevron

--- TourismVillageFrontend/main.js
+++ TourismVillageFrontend/main.js
@@ -1,20 +1,47 @@
-var tourchevron=document.getElementsByClassName("tourchevron")
+var chevron=document.getElementsByClassName("dropdown")
     
     
-var mainnavlink=document.getElementsByClassName("mainnavlink");
+var mainnavlink=document.querySelectorAll(".mainnavlink");
 
 
-
-mainnavlink[0].addEventListener("click", ()=>{
-    if(tourchevron[0].classList.contains("fa-rotate-180")){
-
-        tourchevron[0].classList.remove("fa-rotate-180")    
-    }
-    else{
-        tourchevron[0].classList.add("fa-rotate-180")            
-    }
+console.log(`here is the list of the chevron  ${chevron}`)
+console.log(chevron)
+console.log( `here is the list for the mainnavlink ${ mainnavlink} `)
+console.log(mainnavlink)
     
     
-})
+
+for(i=0; i<mainnavlink.length; i++){
+    mainnavlink[i].addEventListener("click", ()=>{
+        console.log(`here is the clicked shit  ${mainnavlink[i]}   `)
+    })
+
+}
+
+// mainnavlink[i].addEventListener("click", ()=>{   
+//     console.log("here is the i for chevron  " + i)
+
+//     console.log("here is the i for mainnavlink + "+ i) 
+
+//     if(mainnavlink[].classList.contains("fa-rotate-180")){
+//         // chevron[i].classList.remove("dropdown")
+//         chevron[count].style.transition="all 1s ease"          
+//         chevron[count].classList.remove("fa-rotate-180") 
+
+//     }
+
+//     else{
+//         // chevron[i].classList.add("dropdown")
+//         chevron[count].style.transition="all 1s ease"                   
+//         chevron[count].classList.add("fa-rotate-180")  
+
+//     }
+
+
+// })
+
+    
+    
+
 
 
