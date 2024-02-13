461df0ee0e91efc043237ffab143e97de6e6a366
Yegon Kip <KY@Germanschool.local>
2024-02-13 16:10:37 +0300

    just done with navigation header for full screen

--- TourismVillageFrontend/main.js
+++ TourismVillageFrontend/main.js
@@ -1,14 +1,42 @@
 var chevron=document.getElementsByClassName("dropdown")
     
     
-var mainnavlinklist=document.getElementsByClassName("mainnavlink");
+var mainnavlinklist=document.getElementsByClassName("mainnavlinklisted");
 
     
 
 var mainnavlink=[...mainnavlinklist];
 
 mainnavlink.forEach((currentvalue, index,array)=>{
-    currentvalue.addEventListener("click", ()=>{
+    currentvalue.addEventListener("mouseenter", ()=>{
+     
+        console.log("lets see what these guys give out");
+        console.log(currentvalue)
+        
+            
+        if(chevron[index].classList.contains("fa-rotate-180")){
+            // chevron[i].classList.remove("dropdown")
+            chevron[index].style.transition="all 1s ease"          
+            chevron[index].classList.remove("fa-rotate-180") 
+            // chevron[index].style.transform="rotate(180deg)"
+            chevron[index].style.color="green";
+
+
+        }
+        else{
+            // chevron[i].classList.add("dropdown")
+            chevron[index].style.transition="all 1s ease"                   
+            chevron[index].classList.add("fa-rotate-180")  
+            chevron[index].style.color="blue";
+
+        }
+            
+        })
+
+}, )
+
+mainnavlink.forEach((currentvalue, index,array)=>{
+    currentvalue.addEventListener("mouseleave", ()=>{
      
         console.log("lets see what these guys give out");
         console.log(currentvalue)
