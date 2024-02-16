add38d5e9520ded3e294a41ede699ca5043cb962
Yegon Kiprotich <meshcode75@gmail.com>
2024-02-16 04:51:43 +0300

    navheader to  make it responsive nervt mich

--- TourismVillageFrontend/main.js
+++ TourismVillageFrontend/main.js
@@ -1,6 +1,11 @@
+//on load 
 window.onload = ()=>{
     windowWidth();
 }
+
+
+//Meadi queries 
+var mediaQueries=["450", "768", "992", "1200"]
 
 function windowWidth(){
     var windowWidth = window.innerWidth;
@@ -9,17 +14,31 @@ function windowWidth(){
     return windowWidth
 }
 
-var mediaQueries=["450", "768", "992", "1200" ]
-
+//importing Elements from HTML
 var chevron=document.getElementsByClassName("dropdown")
 var mainnavlinklist=document.getElementsByClassName("mainnavlinklisted");
 var mainnavlinks= document.getElementsByClassName("mainnavlink");    
 
-var bigcollapse
+var bigcollapse=document.getElementsByClassName("bigCollapsediv");
+var collapsedivs=document.getElementsByClassName("collapsediv");
+var bigcollapsedivcolumns=document.getElementsByClassName("bigcollapsedivcolumns");
+var bigcollapsecolumheaders=document.getElementsByClassName("bigcollapsecolumheader")
+
+console.log(bigcollapse);
+console.log(collapsedivs);
+console.log(bigcollapsedivcolumns);
     
 
 
 var mainnavlink=[...mainnavlinklist];
+
+if(innerWidth < mediaQueries[0]){
+
+
+}
+else{
+
+}
 
 mainnavlink.forEach((currentvalue, index,array)=>{
     var events= ["mouseenter", "mouseclick", "mouseleave"]
@@ -30,12 +49,14 @@ mainnavlink.forEach((currentvalue, index,array)=>{
     
     
 
-    currentvalue.addEventListener("mouseenter", ()=>{
+    currentvalue.addEventListener("mouseover", ()=>{
      
 
         console.log("lets see what these guys give out");
         console.log(currentvalue)
         
+        
+
         // var clickedornot= currentvalue.isclicked()
         // console.log(clickedornot);
         
@@ -55,13 +76,37 @@ mainnavlink.forEach((currentvalue, index,array)=>{
             chevron[index].style.color="blue";
 
         }
+
+
+        mainnavlinks[index].addEventListener("click", ()=>{
+            
+            // var clickedornot= currentvalue.isclicked()
+            // console.log(clickedornot);
+            
+            if(chevron[index].classList.contains("fa-rotate-180")){
+                // chevron[i].classList.remove("dropdown")
+                chevron[index].style.transition="all 0.1s ease"          
+                chevron[index].classList.remove("fa-rotate-180") 
+                // chevron[index].style.transform="rotate(180deg)"
+                chevron[index].style.color="green";
+
+
+            }
+            else{
+                // chevron[i].classList.add("dropdown")
+                chevron[index].style.transition="all 0.1s ease"                   
+                chevron[index].classList.add("fa-rotate-180")  
+                chevron[index].style.color="blue";
+
+            }
+        })
             
         })
 
 }, )
 
 mainnavlink.forEach((currentvalue, index,array)=>{
-    currentvalue.addEventListener("mouseleave", ()=>{
+    currentvalue.addEventListener("mouseout", ()=>{
      
         console.log("lets see what these guys give out");
         console.log(currentvalue)
@@ -69,7 +114,7 @@ mainnavlink.forEach((currentvalue, index,array)=>{
             
         if(chevron[index].classList.contains("fa-rotate-180")){
             // chevron[i].classList.remove("dropdown")
-            chevron[index].style.transition="all 1s ease"          
+            chevron[index].style.transition="all 0.1s ease"          
             chevron[index].classList.remove("fa-rotate-180") 
             // chevron[index].style.transform="rotate(180deg)"
             chevron[index].style.color="green";
@@ -78,11 +123,12 @@ mainnavlink.forEach((currentvalue, index,array)=>{
         }
         else{
             // chevron[i].classList.add("dropdown")
-            chevron[index].style.transition="all 1s ease"                   
+            chevron[index].style.transition="all 0.1s ease"                   
             chevron[index].classList.add("fa-rotate-180")  
             chevron[index].style.color="blue";
 
         }
+
             
         })
 
