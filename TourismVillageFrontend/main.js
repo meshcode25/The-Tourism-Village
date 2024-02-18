fced0f30bd976d52ac94f876e7021476e70e0de5
Yegon Kiprotich <meshcode75@gmail.com>
2024-02-18 22:31:04 +0300

    I think I have finally cracked the code to mobile responsiveness for navigation bar

--- TourismVillageFrontend/main.js
+++ TourismVillageFrontend/main.js
@@ -21,17 +21,22 @@ var mainnavlinks= document.getElementsByClassName("mainnavlink");
 
 var bigcollapse=document.getElementsByClassName("bigCollapsediv");
 var collapsedivs=document.getElementsByClassName("collapsediv");
-var collapsingdiv=document.getElementsByClassName("showing")
-var bigcollapsedivcolumns=document.getElementsByClassName("bigcollapsedivcolumns");
-var bigcollapsecolumheaders=document.getElementsByClassName("bigcollapsecolumheader")
+var collapsingdiv=document.getElementsByClassName("showing");
+var bigcollapsecolumncontainer=document.getElementsByClassName("bigcollapsecolumncontainer");
+var bigcollapsecolumheader=document.getElementsByClassName("bigcollapsecolumheader");
+var showingcolumn=document.getElementsByClassName("showingcolumn");
+
+
 
 console.log(bigcollapse);
 console.log(collapsedivs);
-console.log(bigcollapsedivcolumns);
     
 
 
 var mainnavlink=[...mainnavlinklist];
+var bigcollapsecolumnheaders=[...bigcollapsecolumheader]
+var showingcolumns=[...showingcolumn]
+var bigcollapsecolumncontainers=[...bigcollapsecolumncontainer]
 
 if(innerWidth < mediaQueries[0]){
 
@@ -168,7 +173,39 @@ mainnavlink.forEach((currentvalue, index,array)=>{
 }, )
   
 
+bigcollapsecolumncontainers.forEach((currentvalue,index, array)=>{
 
+
+    currentvalue.addEventListener("click", ()=>{
+
+        console.log("here is the current value")  
+        console.log(currentvalue)
+
+        if(chevron[index].classList.contains("fa-rotate-180")){
+                
+            chevron[index].style.transition="all 1s ease"          
+            chevron[index].classList.remove("fa-rotate-180") 
+            
+            chevron[index].style.color="green";
+            
+
+            console.log('Bigcollapsedivs has been I the cheveron have been clicked')
+
+            showingcolumn[index].classList.add("collapsingcolumn")
+
+        }
+        else{
+            chevron[index].style.transition="all 1s ease"                   
+            chevron[index].classList.add("fa-rotate-180")  
+            chevron[index].style.color="blue";
+
+
+            console.log('Bigcollapsedivs has been I the cheveron have been clicked')
+            showingcolumn[index].classList.remove("collapsingcolumn")
+
+          }
+    })
+})
 
     
     
